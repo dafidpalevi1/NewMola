2435b0991351825a74776e1b37c0f7f82c4451bc
dafid palevi <43629045+dafidpalevi1@users.noreply.github.com>
2022-01-18 22:10:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,9 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
 	container.classList.remove("sign-up-mode");
 });
+
+if ( carousel) {
+    caraousel.addEventListener('click', ()=>) {
+        nav.classList.remove('active');
+    })
+}
